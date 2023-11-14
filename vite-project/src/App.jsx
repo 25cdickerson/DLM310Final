16106cb3867a1e8bca777a27cc70246f70bb6a74
Christopher (Preston) Dickerson <cdickerson39@yahoo.com>
2023-11-14 12:10:55 -0500

    Added some more cases

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,13 +9,17 @@ import fire from './photos/detention_center_fire.png';
 import laBestia from './photos/La_Bestia.jpg';
 import bus from './photos/bus.jpg';
 import rioGrande from './photos/rio_grande_cross.png';
+import truck from './photos/truck_smuggling.jpg';
+import crash from './photos/truck_crash.png';
+import waterJug from './photos/water_jug.jpg';
+import trainDerail from './photos/train_derail.jpg';
 
 function App() {
 
 
   return (
     <>
-      <Parallax pages={6}>
+      <Parallax pages={10}>
             <ParallaxLayer
               speed={1}
               factor={1}
@@ -54,6 +58,16 @@ function App() {
                   textAlign: 'center'
               }}>
                 <MissingMigrants></MissingMigrants>
+              </ParallaxLayer>
+              <ParallaxLayer style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'flex-end',
+                  alignItems: 'center',
+                  color: '#3e2723',
+                  textAlign: 'center'
+              }}>
+                <h2>Here are some of their stories</h2>
               </ParallaxLayer>
           </ParallaxLayer>
           <ParallaxLayer
@@ -172,7 +186,132 @@ function App() {
                 <br></br>
               </ParallaxLayer>
           </ParallaxLayer>
-          <p>Source <a href='https://www.usnews.com/news/world/articles/2023-03-28/factbox-deadly-tragedies-involving-migrants-crossing-mexico'>U.S. News</a></p>
+          <ParallaxLayer
+              offset={6} 
+              speed={1}
+              style={{
+                backgroundImage: `url(${truck})`,
+                backgroundSize: 'cover',
+                /* Photo from https://nbc-2.com/news/national-world/2022/06/30/4-people-have-been-charged-in-the-texas-semitruck-smuggling-operation-that-left-53-migrants-dead/ */
+              }}>
+              <ParallaxLayer 
+                speed={0.95}
+                offset={2.99} 
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  color: '#3e2723',
+                  textAlign: 'center'
+              }}>
+                <h2>Truck smuggling incident in Texas, June 2022</h2>
+                <br></br>
+                <p>Fifty-three migrants from Mexico, Guatemala, Honduras and El Salvador died in a sweltering tractor trailer in Texas in the deadliest migrant-trafficking incident on record in the U.S.</p>
+                <br></br>
+              </ParallaxLayer>
+          </ParallaxLayer>
+          <ParallaxLayer
+              offset={5} 
+              speed={1}
+              style={{
+                backgroundImage: `url(${rioGrande})`,
+                backgroundSize: 'cover',
+                /* Photo from https://nypost.com/2021/05/10/video-shows-illegal-immigrants-streaming-across-rio-grande/ */
+              }}>
+              <ParallaxLayer 
+                speed={0.95}
+                offset={2.2} 
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  color: '#3e2723',
+                  textAlign: 'center'
+              }}>
+                <h2>Rio Grande drowning, September 2022</h2>
+                <br></br>
+                <p>Nine migrants died and 37 were rescued as they tried to cross the Rio Grande River into the United States.</p>
+                <br></br>
+              </ParallaxLayer>
+          </ParallaxLayer>
+          <ParallaxLayer
+              offset={7} 
+              speed={1}
+              style={{
+                backgroundImage: `url(${crash})`,
+                backgroundSize: 'cover',
+                /* Photo from https://www.dailynews.com/2021/03/02/hospital-suv-carrying-27-crashes-with-semitruck-killing-15/ */
+              }}>
+              <ParallaxLayer 
+                speed={0.95}
+                offset={3.1} 
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  color: '#3e2723',
+                  textAlign: 'center'
+              }}>
+                <h2>Truck crash in southern Mexico, December 2021</h2>
+                <br></br>
+                <p>Fifty-five people, nearly all Guatemalans, were killed after a truck carrying an estimated 166 migrants crashed in Mexico&apos;s southern Chiapas state, in one of the deadliest migrant tragedies in Mexico of the last decade.</p>
+                <br></br>
+              </ParallaxLayer>
+          </ParallaxLayer>
+          <ParallaxLayer
+              offset={8} 
+              speed={1}
+              style={{
+                backgroundImage: `url(${waterJug})`,
+                backgroundSize: 'cover',
+                /* Photo from https://www.dailysignal.com/2015/02/02/border-patrol-union-leader-blasts-washington-middle-political-football/ */
+              }}>
+              <ParallaxLayer 
+                speed={0.95}
+                offset={3.6} 
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'flex-end',
+                  alignItems: 'center',
+                  color: '#3e2723',
+                  textAlign: 'center'
+              }}>
+                <h2>Dehydration deaths on Texas border, June 2019</h2>
+                <br></br>
+                <p>Seven migrants, including a woman, two babies and a toddler, died from extreme summer heat and dehydration after crossing the Mexican border into Texas.</p>
+                <br></br>
+              </ParallaxLayer>
+          </ParallaxLayer>
+          <ParallaxLayer
+              offset={9} 
+              speed={1}
+              style={{
+                backgroundImage: `url(${trainDerail})`,
+                backgroundSize: 'cover',
+                /* Photo from https://www.documentingreality.com/forum/f10/train-derailment-tabasco-mexico-132612/ */
+              }}>
+              <ParallaxLayer 
+                speed={0.95}
+                offset={4} 
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  color: '#3e2723',
+                  textAlign: 'center'
+              }}>
+                <h2>Tabasco train derailment, August 2013</h2>
+                <br></br>
+                <p>At least six people were killed when a cargo train nicknamed &apos;La Bestia&apos; (The Beast) derailed in the state of Tabasco. Many migrants have fallen to their deaths or suffered injuries on the train while hitching a ride to the U.S. border.</p>
+                <br></br>
+                <p>Source <a href='https://www.usnews.com/news/world/articles/2023-03-28/factbox-deadly-tragedies-involving-migrants-crossing-mexico'>U.S. News</a></p>
+              </ParallaxLayer>
+          </ParallaxLayer>
       </Parallax>
     </>
   );
